6bfff850af29e91e695d9d7ed7ce6ba6f4cc3428
Ahmed123567 <amabdo00001@gmail.com>
2023-07-08 17:33:40 +0300

    remove unused uses

--- app/Http/Controllers/back/AutoDoctorController.php
+++ app/Http/Controllers/back/AutoDoctorController.php
@@ -2,24 +2,9 @@
 
 namespace App\Http\Controllers\back;
 
-use Illuminate\Http\Request;
-use App\Events\CovidCheckRan;
 use App\Http\Controllers\Controller;
-
-use function Pest\Laravel\json;
-use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Process;
-use Illuminate\Support\Facades\Storage;
 use App\Http\Requests\CovidCheckRequest;
-use Symfony\Component\Process\ExecutableFinder;
-use Symfony\Component\Console\Output\BufferedOutput;
-use Symfony\Component\Process\Process as ProcessProcess;
-use Illuminate\Process\Exceptions\ProcessFailedException;
-
-use Symfony\Component\Process\Exception\ProcessFailedException as ExceptionProcessFailedException;
 
 class AutoDoctorController extends Controller
 {
